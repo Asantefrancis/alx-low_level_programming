4d23f3aa60933a47db76d2e29c74246f8c8161a0
asantefrancis <nicecake2000gmail.com>
2023-02-16 17:18:20 -0800

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
+
 /**
  * main - Entry point
  *
- * Return: Always 1;
+ * Return: Always (success)
 */
 int main(void)
 {
-	fwrite(STDOUT_FILENO, and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n);
+	fprintf(stderr,
+		       "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 	return (1);
 }
