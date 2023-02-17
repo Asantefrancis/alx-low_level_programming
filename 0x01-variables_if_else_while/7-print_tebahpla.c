9ccf47e3b11f17e597dee65236847246eb6fcecf
asantefrancis <nicecake2000gmail.com>
2023-02-17 15:28:05 -0800

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main -main entry
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'z' ; i >= 'a' ; i--)
+		putchar(i);
+	putcahr('\n');
+	return (0);
+}
