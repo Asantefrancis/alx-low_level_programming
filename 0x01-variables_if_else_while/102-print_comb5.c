86af56b400bf35f4bd14433d18fb2b0d411b0a3c
asantefrancis <nicecake2000gmail.com>
2023-02-17 18:21:53 -0800

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,19 +8,21 @@ int main(void)
 {
 	int i, d;
 
-	for (i = 0; i <= 98; i++)
+	for (i = 0; i < 100; i++)
 	{
-		for (d = 1 + 1; d <= 99; d++)
+		for (d = 0 ; d < 100; d++)
 		{
-			putchar((i / 10) + '0');
-			putchar((i % 10) + '0');
-			putchar(' ');
-			putchar((d / 10) + '0');
-			putchar((d % 10) + '0');
-
-			if (i == 98 && d == 99)
-			putchar(',');
-			putchar(' ');
+			if (i < d)
+			{
+				putchar((i / 10) + '48');
+				putchar((i % 10) + '48');
+				putchar(' ');
+				putchar((d / 10) + '48');
+				putchar((d % 10) + '48');
+				if (i != 98 && d != 99)
+					putchar(',');
+					putchar(' ');
+			}
 		}
 	}
 	putchar('\n');
