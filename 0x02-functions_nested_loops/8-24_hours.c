7acc9db4932b00b1d3f0ab9a554c2521c54b32fb
asantefrancis <nicecake2000gmail.com>
2023-02-21 15:10:16 -0800

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * jack_bauer - function that prints 24hrs time
+ *
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int i;
+	int d;
+
+	for (i = 0; i < 24; i++)
+	{
+		for (d = 0; d < 60; d++)
+		{
+			_putchar(i / 10 + '0');
+			_putchar(i % 10 + '0');
+			-putchar(':');
+			_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			_putchar('\n');
+		}
+	}
+}
+
