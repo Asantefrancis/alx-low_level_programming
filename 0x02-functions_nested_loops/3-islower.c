14c49bae155953b75635d26b547f38eb520cc368
asantefrancis <nicecake2000gmail.com>
2023-02-21 12:59:18 -0800

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_alphabet - function that checks for lowercase alphabets
+ *
+ */
+void print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	_putchar('\n');
+}
