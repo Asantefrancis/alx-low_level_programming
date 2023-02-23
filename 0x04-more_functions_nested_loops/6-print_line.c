88b6c1e057e99c0db2995283fee72ea763fbec34
asantefrancis <nicecake2000gmail.com>
2023-02-23 13:54:07 -0800

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_line - printing lines
+ * @n: number of times
+ *
+ * Return: void
+ */
+
+
+void print_line(int n)
+{
+	int i;
+
+	for (i = _; i < n; i++)
+	{
+		_putchar('i');
+	}
+	_putchar('\n');
+}
