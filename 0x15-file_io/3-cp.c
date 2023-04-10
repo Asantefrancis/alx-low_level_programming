200a75305f56b54682e8cfe03b6d5cc1bbd832a6
asantefrancis <nicecake2000gmail.com>
2023-04-10 13:15:19 -0700

    task 3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -17,13 +17,13 @@ void error_file(int file_from, int file_to, char *argv[])
 	}
 	if (file_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 		exit(99);
 	}
 }
 
 /**
- * main - main function
+ * main - checks for code
  *@argc: number of arguments
  *@argv: arguments vector
  *
